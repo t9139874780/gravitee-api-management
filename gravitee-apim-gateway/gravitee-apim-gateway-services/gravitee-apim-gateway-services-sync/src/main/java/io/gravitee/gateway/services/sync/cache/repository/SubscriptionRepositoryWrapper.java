af03b679d866036af4ea6e3caa813bc218162358
Guillaume Cusnieux <guillaume.cusnieux@graviteesource.com>
2021-12-06 14:57:33 +0100

    feat(subscriptions): add findReferenceIdsOrderByNumberOfSubscriptions

--- gravitee-apim-gateway/gravitee-apim-gateway-services/gravitee-apim-gateway-services-sync/src/main/java/io/gravitee/gateway/services/sync/cache/repository/SubscriptionRepositoryWrapper.java
+++ gravitee-apim-gateway/gravitee-apim-gateway-services/gravitee-apim-gateway-services-sync/src/main/java/io/gravitee/gateway/services/sync/cache/repository/SubscriptionRepositoryWrapper.java
@@ -77,6 +77,11 @@ public class SubscriptionRepositoryWrapper implements SubscriptionRepository {
     }
 
     @Override
+    public Set<String> findReferenceIdsOrderByNumberOfSubscriptions(SubscriptionCriteria criteria) throws TechnicalException {
+        throw new IllegalStateException();
+    }
+
+    @Override
     public Set<Subscription> findAll() throws TechnicalException {
         throw new IllegalStateException();
     }
